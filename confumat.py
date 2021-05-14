97acce9b80ce2779c7db2c3ced6d684e84f34358
RoopSaiPavanTej <60775372+RoopSai-PavanTej@users.noreply.github.com>
2021-05-14 11:10:50 +0530

    Confusion Matrix

---
+++ confumat.py
@@ -0,0 +1,29 @@
+import pandas as pb
+import seaborn as sb
+import tensorflow as tf
+
+
+model=tf.keras.models.load_model("Data_Set\Saved_Model\ResNet50_rice.h5")
+filenames=test_generator.filenames
+nb_samples=len(test_generator)
+y_prob=[]
+y_act=[]
+test_generator.reset()
+for _ in range(nb_samples):
+    X_test,Y_test=test_generator.next()
+    y_prob.append(model.predict(X_test))
+    y_act.append(Y_test)
+
+
+predicted_class=[list(train_generator.class_indices.key())[i.argmax()] for i in y_prob]
+actual_class=[list(train_generator.class_indices.keys())[i.argmax()] for i in y_act]
+
+
+out_df=pd.DataFrame(np.vstack([predicted_class,actual_class]).T,columns=['predicted_class','actual_class'])
+confusion_matrix=pd.crosstab(out_df['actual_class'],out_df['predicted_class'],rownames=['Actual'],colnames=['Predicted'])
+
+
+sn.heatmap(confusion_matrix,cmap=' Blues', annot=True , fmt='d')
+plt.show()
+print('test accuracy: {}'.format((np.diagonal(confusion_matrix).sum()/confusion_matrix.sum().sum()*100)))
+
